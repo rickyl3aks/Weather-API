2c5e1ef342a7dc426fd0a9bb58741b480c7b5d6c
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-10-07 23:14:50 +0200

    weather API

---
+++ main.js
@@ -0,0 +1,96 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const img = document.querySelector(".icon");
+const city = document.querySelector(".city");
+const country = document.querySelector(".country");
+const icon = document.querySelector(".icon");
+const temp = document.querySelector(".temp");
+const weather = document.querySelector(".weather");
+
+btn.addEventListener("click", (e) => {
+  //prevent default CHECK AGAIN!!!
+  e.preventDefault();
+  getWeather(e);
+});
+function getWeather(e) {
+  //get API
+  let api =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    input.value +
+    "&APPID=6cdb1a2bc26117586f24e1ce19ff912b&units=metric";
+  //fetch API
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      //get country
+      country.innerHTML = `(${data.sys.country})`;
+      //get icon
+      icon.innerHTML = `<img src="icons/${data.weather[0].icon}.png"/>`;
+      //get city
+      city.textContent = data.name;
+      //get temperature
+      temp.innerHTML = `${Math.ceil(data.main.temp)}°`;
+      //get weather
+      weather.innerHTML = data.weather[0].main;
+    });
+  noCity();
+}
+
+function noCity() {
+  if (404) {
+    temp.innerHTML = "";
+    country.innerHTML = "";
+    weather.innerHTML = "";
+    icon.innerHTML = `<img src="icons/unknown.png" />`;
+    city.textContent = "Please insert a city";
+  }
+}
+
+/*
+//jquery
+btn.addEventListener("click", function weather(e) {
+  e.preventDefault();
+  $.getJSON(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      input.value +
+      "&APPID=6cdb1a2bc26117586f24e1ce19ff912b&units=metric",
+    function (data) {
+      //get icon
+      var icon = `<img src="icons/${data.weather[0].icon}.png"/>`;
+
+      //get temperature
+      var temp = data.main.temp;
+      //get weather
+      var weather = data.weather[0].main;
+      //get country
+      var country = data.sys.country;
+      //get city
+      var city = data.name;
+
+      //append icon
+      img.innerHTML = icon;
+      //append city
+      $(".city").html(city);
+
+      //append country
+      $(".country").html(` (${country})`);
+      //append weather
+      $(".weather").html(weather);
+
+      //append temperature
+      $(".temp").html(Math.ceil(temp) + "°");
+    }
+  );
+  noCity();
+});
+
+function noCity() {
+  if (404) {
+    temp.innerHTML = "";
+    country.innerHTML = "";
+    weather.innerHTML = "";
+    icon.innerHTML = `<img src="icons/unknown.png" />`;
+    city.textContent = "Please insert a city";
+  }
+}
+*/
